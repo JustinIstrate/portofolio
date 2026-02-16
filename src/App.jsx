febca665fec91b8a90d9b85db35c07140dd8857a
Istrate Gheorghe-Iustin <justinistrate95@icloud.com>
2026-02-16 20:10:40 +0200

    Setup React, Tailwind and GitHub Actions pipeline

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+    return (
+        <div className="h-screen bg-black text-white flex justify-center items-center">
+            <h1 className="text-5xl font-bold text-blue-500">
+                IT WORKS! 🚀
+            </h1>
+        </div>
+    )
+}
